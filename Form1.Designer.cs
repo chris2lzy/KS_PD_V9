f553793bdecece282a8d969b7fc5f02ad64d2c1f
chris2lzy <kristofers2007arna@gmail.com>
2024-02-27 15:05:25 +0200

    saskaita auglus un kalorijas kopa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.edieni = new System.Windows.Forms.DomainUpDown();
+            this.kalorijas = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.kalorijas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // edieni
+            // 
+            this.edieni.Location = new System.Drawing.Point(30, 73);
+            this.edieni.Name = "edieni";
+            this.edieni.Size = new System.Drawing.Size(120, 20);
+            this.edieni.TabIndex = 0;
+            this.edieni.Text = "ēdieni";
+            // 
+            // kalorijas
+            // 
+            this.kalorijas.Location = new System.Drawing.Point(173, 73);
+            this.kalorijas.Name = "kalorijas";
+            this.kalorijas.Size = new System.Drawing.Size(120, 20);
+            this.kalorijas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kalorijas);
+            this.Controls.Add(this.edieni);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kalorijas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DomainUpDown edieni;
+        private System.Windows.Forms.NumericUpDown kalorijas;
     }
 }
 
